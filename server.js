31ae9e996f65150688bb183b9d5b644f30c1fc34
hiranandanisonal <hiranandanisonal@gmail.com>
2016-09-22 06:49:18 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article1',function(req,res){
 });
 
 app.get('/article2',function(req,res){
-res.send('artcle 2 will be served here');
+res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
 });
 
 app.get('/article3',function(req,res){
