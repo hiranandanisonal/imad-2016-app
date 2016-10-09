5014ba9eda34f057e636d0bce25f5dfebe2d2627
hiranandanisonal <hiranandanisonal@gmail.com>
2016-10-09 15:45:19 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
-console.log('loaded!');
-
-{
-var element = document.getElementById('main');
-element.innerHTML = "new value aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaancsnkcsnakcaslcalscskackscn";
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function(){
+    counter=counter+1;
+    var button=document.getElement('count');
+    span.innerHTML=counter.toString();
 }
-
 
