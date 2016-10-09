c53e5bb6f6603f84b019cd54ec0a89d69b99fb21
hiranandanisonal <hiranandanisonal@gmail.com>
2016-10-09 13:17:09 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,1 @@
 console.log('Loaded!');
-// content change by js
-
-var element = document.getElementById('main-text');
-element.innerHTML='my-webb-app';
-
-// img move by js
-var img=document.getEllementById('move');
-move.onclick=function(){
-   move.style.marginleft='100px' ;
-};
