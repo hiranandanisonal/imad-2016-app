d20f2bb2ac6fc91bfdd6af53822f45f4561eaa83
hiranandanisonal <hiranandanisonal@gmail.com>
2016-10-09 13:30:18 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 console.log('Loaded!');
 
 
-var x = document.getElementById('main-text')
+var x = document.getElementById('main-text');
 
-x.innerHTML='new value';
+x.innerHTML='(new value)';
 
 
 
