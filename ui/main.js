7d94514795f153dd2ce737c340c874f3497cb242
hiranandanisonal <hiranandanisonal@gmail.com>
2016-10-09 13:11:31 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
 console.log('Loaded!');
 // content change by js
 
-var element = document.getElementById("main-text");
-element.innerHTML="my-webb-app";
+var element = document.getElementById('main-text');
+element.innerHTML='my-webb-app';
 
 // img move by js
-var img=document.getEllementById("move");
+var img=document.getEllementById('move');
 move.onclick=function(){
-   move.style.marginleft="100px" ;
+   move.style.marginleft='100px' ;
 };
