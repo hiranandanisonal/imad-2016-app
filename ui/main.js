d0748166f059416af3675608ab65702c30bb1487
hiranandanisonal <hiranandanisonal@gmail.com>
2016-10-10 08:25:06 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,16 @@
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick=function(){
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    var request=new XMLHttpRequest();
+    
+    request.onreadyrtatechange=function(){
+    if(request.readystate == XMLHttpRequest.DONE){
+        if(request.status==200){
+            var counter=request.responsetext;
+        }
+    }    
+    };
+   request.open('GET','http://hiranandanisonal.imad.hasura-app.io/counter',true);
+   request.send(null);
 };
 
