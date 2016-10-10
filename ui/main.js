9e816b4366ed0123c73e4407c7a178d167417cd3
hiranandanisonal <hiranandanisonal@gmail.com>
2016-10-10 08:30:22 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,12 @@ var button=document.getElementById('counter');
 button.onclick=function(){
     var request=new XMLHttpRequest();
     
-    request.onreadyrtatechange=function(){
+    request.onreadystatechange=function(){
     if(request.readystate == XMLHttpRequest.DONE){
         if(request.status==200){
             var counter=request.responsetext;
+            var span=document.getElementById('count');
+            span.innerHTML=counter.toString();
         }
     }    
     };
