43bc72a0203bcfe7893f93344956c11fd8989261
hiranandanisonal <hiranandanisonal@gmail.com>
2016-11-04 22:46:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -44,6 +44,10 @@ var articles={
    <iframe src="https://www.facebook.com/plugins/follow.php?href=https%3A%2F%2Fwww.facebook.com%2Fdvirus.sh&width=450&height=80&layout=standard&size=small&show_faces=true&appId" width="450" height="80" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
       
       <a href="https://twitter.com/hiranandanison1/media"><img src="http://www.iconarchive.com/download/i58879/dakirby309/windows-8-metro/Web-Twitter-alt-2-Metro.ico" width="80px" height="80px" /></a>
+      
+      <a href="https://hsonali97.blogspot.in/2016_10_01_archive.html"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Blogger.svg/1024px-Blogger.svg.png" /></a>
+      
+      <a href="https://plus.google.com/106015111898465232878"><img src="https://freeiconshop.com/files/edd/google-plus-flat.png" /></a>
     
     </p> `
 },
