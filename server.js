835e9adb11c06a18eac5d205c61e868220fccb6d
hiranandanisonal <hiranandanisonal@gmail.com>
2016-11-04 11:02:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ var articles={
     content:`
         <p>
         <a href="/">Home</a> |
-        <a href="http://hiranandanisonal.imad.hasura-app.io/about us" target="_sameframe">about us</a> |
+        <a href="http://hiranandanisonal.imad.hasura-app.io/about me" target="_sameframe">about me</a> |
       
-        <a href="http://hiranandanisonal.imad.hasura-app.io/say hello!">say hello</a> |
+        <a href="http://hiranandanisonal.imad.hasura-app.io/say hello!">contact me</a> |
       
         <a href="http://hiranandanisonal.imad.hasura-app.io/shop">shop</a>
        
@@ -37,9 +37,9 @@ var articles={
     content:`
     <p>
         <a href="/">Home</a> |
-        <a href="http://hiranandanisonal.imad.hasura-app.io/about us" target="_sameframe">about us</a> |
+        <a href="http://hiranandanisonal.imad.hasura-app.io/about us" target="_sameframe">about me</a> |
       
-        <a href="http://hiranandanisonal.imad.hasura-app.io/say hello!">say hello</a> |
+        <a href="http://hiranandanisonal.imad.hasura-app.io/say hello!">contact me</a> |
       
         <a href="http://hiranandanisonal.imad.hasura-app.io/shop">shop</a>
        
