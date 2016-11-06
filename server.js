2f0965e2b52cb65236cf2efe600c3141158eb25b
hiranandanisonal <hiranandanisonal@gmail.com>
2016-11-06 14:08:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -220,6 +220,15 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+app.get('/ui/blogger.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'blogger.png'));
+});
+app.get('/ui/googleplus.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'googleplus.jpg'));
+});
+app.get('/ui/twitter.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'twitter.ico'));
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
