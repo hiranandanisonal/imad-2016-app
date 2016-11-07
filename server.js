5f658d7680cc74f6e7aef1c6b6983ba79fd9df70
hiranandanisonal <hiranandanisonal@gmail.com>
2016-11-07 16:17:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,9 +76,9 @@ app.get('/', function (req, res) {
   
 });
 
-function hash (input){
+function hash (input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return return.hash.toString('hex');
+    return hashed.toString('hex');
 }
 app.get('/hash/:input',function(req,res)
 {
