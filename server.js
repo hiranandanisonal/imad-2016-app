3918c7b172fd27183fa532e30e20c5a936b79038
hiranandanisonal <hiranandanisonal@gmail.com>
2016-11-07 16:27:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/hash/:input',function(req,res)
 {
     var hashedString=hash(res.params.input,'this-is-same-random-string');
     res.send(hashedString);
-})
+});
 
 var pool=new Pool(config);
 app.get('/test-db',function(req,res)
