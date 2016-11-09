0ec793ca63256b5c50547e58f6c7e03912b69d5f
hiranandanisonal <hiranandanisonal@gmail.com>
2016-11-09 17:17:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,7 @@ submit.onclick=function(){
     console.log(username);
     console.log(password);
    request.open('POST','http://hiranandanisonal.imad.hasura-app.io/login',true);
-   request.setRequestHeader('Content-type','application/json');
+   request.setRequestHeader('Content-Type','application/json');
    request.send(JSON.stringify({username:username,password:password}));
 };
 
